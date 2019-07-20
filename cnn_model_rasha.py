7a3d564411897f4923dbdb826a2c9370a574e851
Soumya Suvra Ghosal <52136583+Soumya1612-Rasha@users.noreply.github.com>
2019-07-20 03:31:40 -0400

    Add files via upload

---
+++ cnn_model_rasha.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+from keras.model import Sequential
+from keras.layers import Dense,Conv2D,MaxPooling2D,Flatten,BatchNormalization,Dropout
+from sklearn.model_selection import KFold
+np.random.seed(Cfg.random_state)
+from Configuration import Cfg
+import logging
+from time import perf_counter
+import os.path as path
+
+class CNN_Model_rasha(object):
+    def _init_(self):
+        self.trained=0
+        self.logger = logging.getLogger(self.__class__.__name__)
+    
+    def create_model_static_rasha(self,x_train):
+        model = Sequential()
+        model.add(Conv2D(filters= 32, kernel_size=(3, 3),padding="same", activation='relu',kernel_initializer='uniform', input_shape=(Cfg.image_width, Cfg.image_height, Cfg.channels)))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+        model.add(Conv2D(filters=64,kernel_size=(3, 3), activation='relu', kernel_initializer='uniform'))
+	    model.add(Conv2D(filters=64,kernel_size=(3, 3), activation='relu', kernel_initializer='uniform'))
+	    model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(100, activation='relu',kernel_initializer='uniform'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.35))
+        model.add(Dense(Cfg.num_classes, activation='softmax'))
+        model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])     #compile model
+        self.model=model
+        self.trained=1
+        
+    def fit_model_rasha(self, dataX, dataY, n_folds:int=5):
+	    scores, histories = list(), list()
+	    kfold = KFold(n_folds, shuffle=True, random_state=Cfg.random_state) 	# prepare cross validation
+        for train_ix, test_ix in kfold.split(dataX):     	# enumerate splits
+            trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]  # select rows for train and test
+		    if self.trained==1 :
+                history = self.model.fit(trainX, trainY, epochs=cfg.epochs, batch_size=Cfg.batch_size, validation_data=(testX, testY), verbose=0) # fit model
+    		else:
+                raise Exception('Model is not trained')
+            _, acc = model.evaluate(testX, testY, verbose=0)   # evaluate model
+            '> %.3f' % (acc * 100.0))
+    		scores.append(acc)   # stores scores
+            histories.append(history) #histories scores
+        return scores,histories
+    
+    def predict_data_rasha(self,x_test):
+        if self.trained=1:
+            return(self.model.predict(x_test))
+        else:
+            raise Exception('Model is not trained')
+    
+    def search_best_model_rasha(self,data):
+        
+        """
+        Search the best model using hyperopt & stock best model 
+        input data: (X_train, X_val, X_test, y_train, y_val, y_test)
+        """
+        from hyperopt import Trials, tpe,rand
+        from hyperas import optim
+        
+        t0 = perf_counter() # timer 
+        best_run, best_model,space = optim.minimize(model= create_model_dynamic_rasha, data=data,rseed=Cfg.random_state,algo=tpe.suggest,max_evals=Cfg.evals,eval_space=True,verbose=True,return_space=True)
+        self.model = best_model
+        self.best_run = best_run
+        self.trained=1
+        self.logger.info(' best-run:  '+(str(best_run)))
+        self.logger.info((' The serach of best model took: ')+ (str(round(perf_counter()-t0,5))) + " " +(".s") )
+   
+    def create_model_dynamic_rasha(self,x_train,y_train,x_val,y_val):
+        
+        from hyperas.distributions import choice
+        model = Sequential()
+        model.add(Conv2D(filters= 32, kernel_size=(3, 3),padding="same", activation='relu',kernel_initializer='uniform', input_shape=(x_train.shape[1], x_train.shape[2], 1)))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+        model.add(Conv2D(filters=64,kernel_size=(3, 3), activation='relu', kernel_initializer='uniform'))
+	    model.add(Conv2D(filters=64,kernel_size=(3, 3), activation='relu', kernel_initializer='uniform'))
+	    model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(100, activation='relu',kernel_initializer='uniform'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.35))
+        model.add(Dense(10, activation='softmax'))
+        model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])     #compile model
+        model.fit(x_train,y_train,epochs=cfg.epochs,batch_size={{choice=[32,64,128]}},verbose=1,shuffle='False')
+        score = model.evaluate(x_val, y_val, verbose=0)
+        return {'loss': score, 'status': STATUS_OK, 'model': model}
+    
---
+++ hand_written_character_rasha.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 21:54:21 2019
+
+@author: Soumya Suvra Ghosal
+"""
+from keras.datasets import mnist
+import numpy as np
+import os.path as path
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from cnn_model_rasha import CNN_Model_rasha
+from Configuration import Cfg
+from sklearn.model_selection import train_test_split
+np.random.seed(Cfg.random_state)
+
+def load_dataset_rasha():
+    '''load dataset'''
+	(trainX, trainY),(testX, testY) = mnist.load_data() #Loading the data
+	trainX = trainX.reshape((trainX.shape[0], Cfg.image_width, Cfg.image_height, Cfg,channels))     #reshaping the data
+	testX = testX.reshape((testX.shape[0], Cfg.image_width, Cfg.image_height, Cfg.channels))  
+	trainY = to_categorical(trainY)   #one-hot encoding
+	testY = to_categorical(testY)
+	return trainX, trainY, testX, testY
+
+# scale pixels
+def scale_pixels_rasha(train, test):
+    '''function to scale pixels in 0 to 1 range'''
+	train_norm = train.astype('float32')   #integer to float
+	test_norm = test.astype('float32')
+	train_norm /= 255.0  #Scaling the pixels
+	test_norm  /= 255.0
+	return train_norm, test_norm
+
+
+def accuracy_rasha(test_x, test_y):
+    '''finding accuracy on test data'''
+    result = CNN_rasha.predict_data_rasha(test_x)
+    predicted_class = np.argmax(result, axis=1)
+    true_class = np.argmax(test_y, axis=1)
+    num_correct = np.sum(predicted_class == true_class) 
+    accuracy = float(num_correct)/result.shape[0]
+    return (accuracy * 100)
+
+
+def plot_history_rasha(histories):
+    ''' function to print the learning curves'''
+    for i in range(len(histories)):
+    		plt.subplot(211)
+            title('Cross Entropy Loss')
+            plot(histories[i].history['loss'], color='blue', label='train')
+            plot(histories[i].history['val_loss'], color='orange', label='test')
+            subplot(212)
+            title('Classification Accuracy')
+            plot(histories[i].history['acc'], color='blue', label='train')
+            plot(histories[i].history['val_acc'], color='orange', label='test')
+            plt.show()
+
+def main():
+
+    print("\n[info] checking for 'out' folder if exists")
+    if not path.exists('out'):
+        print("\n[info] out folder not available, creating folder 'out' in the root directory")
+        os.mkdir('out')
+
+    print("\n[INFO] loading data from the dataset into variables")
+    x_train, y_train, x_test, y_test = load_dataset_rasha()
+    x_train, x_test=scale_pixels_rasha(x_train,x_test)
+    CNN_rasha=CNN_Model_rasha() #importing class
+    if Cfg.dynamic==1:                     #finding best model
+         x_test,x_val,y_test,y_val=train_test_split(x_test,y_test,test_size=Cfg.size)
+         x_train,x_test,x_val,y_train,y_test,y_val=data()
+         CNN_rasha.select_best_model_rasha(data)
+    else :
+        CNN_rasha.create_model_static_rasha(x_train)    #using static model
+        
+    [scores,histories]=CNN_rasha.fit_model_rasha(x_train,y_train) #fitting the CNN
+    if Cfg.plotHistory:
+        plot_history_rasha(histories)        # plot learning curves
+    accuracy=accuracy_rasha(x_test,y_test)         #finding accuracy on test data
+    print("Accuracy of the model :"+str(accuracy))    
+
+
+if __name__ == '__main__':
+    main()
